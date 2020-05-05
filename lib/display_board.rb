41dbec81f128b4c3fa0cea739bd66e13213d3b16
Anthony Miguez <anm501@stern.nyu.edu>
2020-05-05 02:40:14 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,9 @@ def display_board
   cell = "   "
   pipe = '|'
   divider = '-----------'
-  print cell, pipe, cell, pipe, cell
+  print cell, pipe, cell, pipe, cell, "\n"
   print divider
-  print cell, pipe, cell, pipe, cell
+  print cell, pipe, cell, pipe, cell, "\n"
   print divider
   print cell, pipe, cell, pipe, cell
 end
